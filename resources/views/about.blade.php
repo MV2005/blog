b4f2eb7d45f2b58c0cdef76bc344a65a7c63318b
MV2005 <marleenvahtra@gmail.com>
2023-10-02 15:51:18 +0300

    commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+
+@extends('layout')
+
+@section('content')
+    <h1 class="text-3xl font-bold underline">
+       About
+    </h1>
+    <button class="btn bg-secondary-content">click me</button>
+@endsection
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+            <h1 class="text-3xl font-bold underline">
+                Hello world!
+        </h1>
+        <button class="btn bg-secondary-content">click me</button>
+@endsection
