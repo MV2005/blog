6d8747169d29401dedde802f7e6b32575c7d2a6f
MV2005 <marleenvahtra@gmail.com>
2023-10-09 15:22:42 +0300

    03.10

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,7 +13,7 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::paginate();
+        $articles = Article::simplePaginate();
         return view('articles.index', compact('articles'));
     }
 
